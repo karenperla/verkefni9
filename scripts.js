566c0e4ba71e06aed01bb05f0dcb7f06a4a8c1f8
Karen <kpk3@hi.is>
2018-11-13 14:33:10 +0000

    setti inn functions init og emptyresults

--- scripts.js
+++ scripts.js
@@ -1,17 +1,36 @@
 // const API_URL = '/example.json?domain=';
 const API_URL = 'https://apis.is/isnic?domain=';
+const domains = document.querySelector(`.domains`);
 
 /**
  * Leit að lénum á Íslandi gegnum apis.is
  */
 const program = (() => {
-  function init() {
+  const results = domains.querySelector(`.results`);
 
+  function init(_form) {
+    _form.addEventListener(`submit`, formHandler);
   }
 
   return {
     init,
   };
+
+  function emptyResults() {
+    while (results.firstChild) {
+      results.removeChild(results.firstChild);
+    }
+  }
+
+  function displayErrorMessage(message) {
+    emptyResults();
+    const span = document.createElement(`span`);
+    span.appendChild(document.createTextNode(message));
+    results.appendChild(span);
+  }
+
+  
+
 })();
 
 document.addEventListener('DOMContentLoaded', () => {
